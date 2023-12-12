fabc64ffa954925ad1de1726bb1d73c852cb9548
DeanPhillips24 <conrickjd3@gmail.com>
2023-12-12 07:12:31 +0000

    platform change

--- assets/js/PlatformO.js
+++ assets/js/PlatformO.js
@@ -1,9 +1,11 @@
-import GameEnv from './GameEnv.js';
 import GameObject from './GameObject.js';
+import GameEnv from './GameEnv.js';
 
 export class PlatformO extends GameObject {
     constructor(canvas, image) {
         super(canvas, image, 0);
+        this.platformX = 0;
+        this.platformY = 0;
     }
 
     // Required, but no update action
@@ -18,22 +20,23 @@ export class PlatformO extends GameObject {
     // Set platform position
     size() {
         // Formula for Height should be on constant ratio, using a proportion of 832
-        const scaledHeight = GameEnv.innerHeight * (60/832);
+        const scaledHeight = GameEnv.innerHeight * (60 / 832);
         // Formula for Width is scaled: scaledWidth/scaledHeight == this.width/this.height
         const scaledWidth = 150;
-        const platformX = .1 * GameEnv.innerWidth;
-        const platformY = (GameEnv.bottom - scaledHeight) - 150;
+        const platformX = 0.1 * GameEnv.innerWidth;
+        const platformY = GameEnv.bottom - scaledHeight - 150;
 
-   // set variables used in Display and Collision algorithms
-   this.bottom = platformY;
-   this.collisionHeight = scaledHeight;
-   this.collisionWidth = scaledWidth;
-   
-   //this.canvas.width = this.width; 
-        //this.canvas.height = this.height;
-        this.canvas.style.width = `{scaledHeight}px`;
+        // Set variables used in Display and Collision algorithms
+        this.bottom = platformY;
+        this.collisionHeight = scaledHeight;
+        this.collisionWidth = scaledWidth;
+
+        // Set canvas styles
+        this.canvas.style.width = `${scaledWidth}px`;
+        this.canvas.style.height = `${scaledHeight}px`;
         this.canvas.style.position = 'absolute';
-        this.canvas.style.left = `{platformY}px`; 
+        this.canvas.style.left = `${platformX}px`;
+        this.canvas.style.top = `${platformY}px`;
     }
 }
 
